4e9a02fab2e6d222483af3d222a05d9484b3ac7a
timas <vas.liwanov@yandex.ru>
2021-10-12 23:19:48 +0300

    fixed phone and birthDay fields

--- src/components/Settings/Main.vue
+++ src/components/Settings/Main.vue
@@ -1,75 +1,80 @@
 <template lang="pug">
-  .settings-main
-    user-info-form-block(:label="$t('name')" :placeholder="$t('entName')" v-model="name" )
-    user-info-form-block(:label="$t('lastName')" :placeholder="$t('entLastName')" v-model="lastName" )
-    user-info-form-block(:label="$t('tel')" :placeholder="$t('entTel')" v-model="phone" phone)
+.settings-main
+  user-info-form-block(:label='$t("name")', :placeholder='$t("entName")', v-model='name')
+  user-info-form-block(:label='$t("lastName")', :placeholder='$t("entLastName")', v-model='lastName')
+  user-info-form-block(:label='$t("tel")', :placeholder='$t("entTel")', v-model='phone', phone)
 
-    .user-info-form__block(v-if="country || isCountryShow")
-      span.user-info-form__label {{ $t('country') }}
-      .user-info-form__wrap.countries(v-click-outside="countriesClose")
-        input.user-info-form__input(
-          type="text"
-          v-model="country"
-          :placeholder="$t('entCountry')"
-          @input="countriesOpen"
+  .user-info-form__block(v-if='country || isCountryShow')
+    span.user-info-form__label {{ $t("country") }}
+    .user-info-form__wrap.countries(v-click-outside='countriesClose')
+      input.user-info-form__input(
+        type='text',
+        v-model='country',
+        :placeholder='$t("entCountry")',
+        @input='countriesOpen'
+      )
+
+      ul.countries__list(v-if='countries.length !== 0 && isCountriesShow')
+        li.countries__item(
+          v-for='item in countries',
+          :key='item.id',
+          tabindex=0,
+          @click='setCountry(item.title)',
+          @keyup.enter='setCountry(item.title)'
+        ) {{ item.title }}
+
+  .user-info-form__block
+    span.user-info-form__label {{ $t("city") }}
+    .user-info-form__wrap.countries(v-click-outside='citiesClose')
+      input.user-info-form__input(
+        type='text',
+        v-model='city',
+        :placeholder='$t("entCity")',
+        @input='citiesOpen',
+        @keyup.enter='setCity(city)'
+      )
+
+      ul.countries__list(v-if='cities.length !== 0 && isCitiesShow')
+        li.countries__item(
+          v-for='item in cities',
+          :key='item.countryId',
+          @click='setCity(item)',
+          @keyup.enter='setCity(item)',
+          tabindex=0
+        ) {{ item.country }}
+
+  .user-info-form__block
+    span.user-info-form__label {{ $t("birthDay") }}
+    .user-info-form__wrap
+      select.select.user-info-form__select.day(v-model='day')
+        option(v-for='d in days', :key='d') {{ d }}
+      select.select.user-info-form__select.month(v-model='month')
+        option(v-for='month in months', :key='month.val', :value='month.val') {{ month.text }}
+      select.select.user-info-form__select.year(v-model='year')
+        option(v-for='i in years', :key='i') {{ i }}
+  .user-info-form__block.user-info-form__block--photo
+    span.user-info-form__label {{ $t("photo") }}
+    .user-info-form__wrap
+      .user-info-form__photo-wrap
+        input#photo.user-info-form__input-photo(
+          type='file',
+          ref='photo',
+          @change='processFile($event)',
+          accept='image/*'
         )
-
-        ul.countries__list(v-if="countries.length !== 0 && isCountriesShow")
-          li.countries__item(
-            v-for="item in countries"
-            :key="item.id"
-            tabindex=0
-            @click="setCountry(item.title)"
-            @keyup.enter="setCountry(item.title)"
-          ) {{item.title}}
-
-    .user-info-form__block
-      span.user-info-form__label {{ $t('city') }}
-      .user-info-form__wrap.countries(v-click-outside="citiesClose")
-        input.user-info-form__input(
-          type="text"
-          v-model="city"
-          :placeholder="$t('entCity')"
-          @input="citiesOpen"
-          @keyup.enter="setCity(city)"
-        )
-
-        ul.countries__list(v-if="cities.length !== 0 && isCitiesShow")
-          li.countries__item(
-            v-for="item in cities"
-            :key="item.countryId"
-            @click="setCity(item)"
-            @keyup.enter="setCity(item)"
-            tabindex=0
-          ) {{item.country}}
-
-    .user-info-form__block
-      span.user-info-form__label {{ $t('birthDay') }}
-      .user-info-form__wrap
-        select.select.user-info-form__select.day(v-model="day")
-          option(v-for="d in days" :key="d") {{d}}
-        select.select.user-info-form__select.month(v-model="month")
-          option(v-for="month in months" :key="month.val" :value="month") {{month.text}}
-        select.select.user-info-form__select.year(v-model="year")
-          option(v-for="i in years" :key="i") {{i}}
-    .user-info-form__block.user-info-form__block--photo
-      span.user-info-form__label {{ $t('photo') }}
-      .user-info-form__wrap
-        .user-info-form__photo-wrap
-          input.user-info-form__input-photo(type="file" ref="photo" id="photo"  @change="processFile($event)" accept="image/*")
-          label.user-info-form__input.user-info-form__input--photo(for="photo")
-            span.setting-main__photo-delete(v-if="photo") {{photo.name}}
-              simple-svg(:filepath="'/static/img/delete.svg'" @click.native.prevent="deletePhoto")
-          button-hover(variant="fill" bordered @click.native="loadPhoto") {{ $t('download') }}
-        .user-info-form__photo-pic(v-if="src")
-          img(:src="src" :alt="name +' ' + lastName")
-    user-info-form-block(:label="$t('myself')" v-model="about" about)
-    .user-info-form__block.user-info-form__block--actions
-      span.user-info-form__label
-      .user-info-form__wrap
-        button-hover(@click.native.prevent="submitHandler") {{ $t('save') }}
-        router-link.settings-main__back(:to="{name: 'Profile'}")
-          button-hover(variant="red" bordered) {{ $t('cancel') }}
+        label.user-info-form__input.user-info-form__input--photo(for='photo')
+          span.setting-main__photo-delete(v-if='photo') {{ photo.name }}
+            simple-svg(:filepath='"/static/img/delete.svg"', @click.native.prevent='deletePhoto')
+        button-hover(variant='fill', bordered, @click.native='loadPhoto') {{ $t("download") }}
+      .user-info-form__photo-pic(v-if='src')
+        img(:src='src', :alt='name + " " + lastName')
+  user-info-form-block(:label='$t("myself")', v-model='about', about)
+  .user-info-form__block.user-info-form__block--actions
+    span.user-info-form__label
+    .user-info-form__wrap
+      button-hover(@click.native.prevent='submitHandler') {{ $t("save") }}
+      router-link.settings-main__back(:to='{ name: "Profile" }')
+        button-hover(variant='red', bordered) {{ $t("cancel") }}
 </template>
 
 <script>
@@ -86,8 +91,8 @@ export default {
     phone: '',
     about: '',
     day: 1,
-    month: { },
-    year: 2000,    
+    month: 0,
+    year: 2000,
     photo: null,
     src: '',
     country: '',
@@ -98,7 +103,7 @@ export default {
     country_city: {
       country: '',
       city: ''
-    },
+    }
   }),
   computed: {
     ...mapGetters('global/storage', ['getStorage']),
@@ -154,21 +159,22 @@ export default {
         { val: 10, text: 'Ноября' },
         { val: 11, text: 'Декабря' }
       ]
-    },
+    }
   },
+
   methods: {
     ...mapActions('global/storage', ['apiStorage']),
     ...mapActions('profile/info', ['apiChangeInfo']),
     ...mapActions('profile/country_city', ['apiCountries', 'apiAllCities']),
     submitHandler() {
-      if (this.src !== this.getInfo.photo && this.src !== '') {
+      if (this.getInfo && this.src !== this.getInfo.photo && this.src !== '') {
         this.apiStorage(this.photo).then(() => {
           this.apiChangeInfo({
             photo_id: this.getStorage && this.getStorage.id,
             first_name: this.name,
             last_name: this.lastName,
-            birth_date: moment([this.year, this.month.val - 1, this.day]).format(),
-            phone: this.phoneNumber,
+            birth_date: moment(new Date(this.year, this.month, this.day)).format(),
+            phone: this.phoneNumber || null,
             about: this.about,
             country: this.country,
             city: this.city
@@ -178,8 +184,8 @@ export default {
         this.apiChangeInfo({
           first_name: this.name,
           last_name: this.lastName,
-          birth_date: moment([this.year, this.month.val - 1, this.day]).format(),
-          phone: this.phoneNumber,
+          birth_date: moment(new Date(this.year, this.month, this.day)).format(),
+          phone: this.phoneNumber || null,
           about: this.about,
           country: this.country,
           city: this.city
@@ -203,7 +209,7 @@ export default {
       this.name = this.getInfo.first_name
       this.lastName = this.getInfo.last_name
       this.src = this.getInfo.photo
-      this.phone = this.getInfo.phone ? this.getInfo.phone.replace(/^[+]?[78]/, "") : "";
+      this.phone = this.getInfo.phone ? this.getInfo.phone.replace(/^[+]?[78]/, '') : ''
       if (this.getInfo.birth_date) {
         this.day = moment(this.getInfo.birth_date * 1000).date()
         this.month = this.months[moment(this.getInfo.birth_date * 1000).month()]
@@ -265,44 +271,44 @@ export default {
   },
   i18n: {
     messages: {
-      "en": {
-        "tel": "Phone:",
-        "entTel": "Enter your phone",
-        "lastName": "Last name:",
-        "entLastName": "Enter last name",
-        "name": "Name:",
-        "entName": "Enter name",
-        "country": "Country:",
-        "entCountry": "Enter country",
-        "city": "City:",
-        "entCity": "Enter city",
-        "birthDay": "Date of Birth:",
-        "photo": "Photo:",
-        "myself": "About myself:",
-        "download": "Download",
-        "cancel": "Cancel",
-        "save": "Save"
+      en: {
+        tel: 'Phone:',
+        entTel: 'Enter your phone',
+        lastName: 'Last name:',
+        entLastName: 'Enter last name',
+        name: 'Name:',
+        entName: 'Enter name',
+        country: 'Country:',
+        entCountry: 'Enter country',
+        city: 'City:',
+        entCity: 'Enter city',
+        birthDay: 'Date of Birth:',
+        photo: 'Photo:',
+        myself: 'About myself:',
+        download: 'Download',
+        cancel: 'Cancel',
+        save: 'Save'
       },
-      "ru": {
-        "tel": "Телефон:",
-        "entTel": "Введите телефон",
-        "lastName": "Фамилия:",
-        "entLastName": "Введите фамилию",
-        "name": "Имя:",
-        "entName": "Введите имя",
-        "country": "Страна:",
-        "entCountry": "Введите страну",
-        "city": "Город:",
-        "entCity": "Введите город",
-        "birthDay": "Дата рождения:",
-        "photo": "Фотография:",
-        "myself": "О себе:",
-        "download": "Загрузить",
-        "cancel": "Отмена",
-        "save": "Сохранить"
+      ru: {
+        tel: 'Телефон:',
+        entTel: 'Введите телефон',
+        lastName: 'Фамилия:',
+        entLastName: 'Введите фамилию',
+        name: 'Имя:',
+        entName: 'Введите имя',
+        country: 'Страна:',
+        entCountry: 'Введите страну',
+        city: 'Город:',
+        entCity: 'Введите город',
+        birthDay: 'Дата рождения:',
+        photo: 'Фотография:',
+        myself: 'О себе:',
+        download: 'Загрузить',
+        cancel: 'Отмена',
+        save: 'Сохранить'
       }
     }
-  },
+  }
 }
 </script>
 
@@ -329,26 +335,25 @@ export default {
 
 .countries {
   position: relative;
+
   &__list {
     position: absolute;
     top: 50px;
     width: 100%;
     max-height: 150px;
     z-index: 10;
-
     overflow-y: scroll;
-
     background-color: #FFFFFF;
     border: 1px solid #c4c4c4;
   }
+
   &__item {
     padding: 5px 20px;
 
     &:hover, &:focus {
-      background-color: rgba(0,0,0,0.3);
+      background-color: rgba(0, 0, 0, 0.3);
       cursor: pointer;
     }
-
   }
 }
 </style>
