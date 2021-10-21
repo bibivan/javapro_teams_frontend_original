5956e63ad83fc46ce90a091aa16d6be70b34a28e
timas <vas.liwanov@yandex.ru>
2021-10-21 22:33:47 +0300

    fixed post date

--- src/components/Search/Filter/News.vue
+++ src/components/Search/Filter/News.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
-  .search-filter
-    .search-filter__block
-      label.search__label(for="search-news-author") Автор:
-      input.search__input(type="text" id="search-news-author" v-model="author")
-    .search-filter__block.time
-      label.search__label Время публикации:
-      select.select.search-filter__select(v-model="date_from")
-        option(value="year") За последний год
-        option(value="month") За последний месяц
-        option(value="week") За последнюю неделю
-    .search-filter__block.tags
-      add-tags(:tags="tags" @change-tags="onChangeTags")
-    .search-filter__block.btn-news
-      button-hover(@click.native="onSearchNews") Применить
+.search-filter
+  .search-filter__block
+    label.search__label(for='search-news-author') Автор:
+    input#search-news-author.search__input(type='text', v-model='author')
+  .search-filter__block.time
+    label.search__label Время публикации:
+    select.select.search-filter__select(v-model='date_from')
+      option(value='year') За последний год
+      option(value='month') За последний месяц
+      option(value='week') За последнюю неделю
+  .search-filter__block.tags
+    add-tags(:tags='tags', @change-tags='onChangeTags')
+  .search-filter__block.btn-news
+    button-hover(@click.native='onSearchNews') Применить
 </template>
 
 <script>
@@ -43,8 +43,11 @@ export default {
         text: this.searchText,
         date_from: moment()
           .subtract(1, this.date_from)
-          .valueOf(),
-        date_to: this.date_to,
+          .format(),
+        // moment()
+        //   .subtract(1, this.date_from)
+        //   .valueOf(),
+        date_to: moment().format(), //this.date_to
         author: this.author
       })
     }
