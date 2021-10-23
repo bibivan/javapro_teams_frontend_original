682879f67620985d7cbf598d43e25171ca873952
timas <vas.liwanov@yandex.ru>
2021-10-23 17:55:18 +0300

    fixed password

--- src/components/FormElements/PasswordField.vue
+++ src/components/FormElements/PasswordField.vue
@@ -8,6 +8,7 @@
     :type='passwordFieldType',
     :autocomplete='autocomplete',
     v-model.trim='password',
+    :autocomplete='autocomplete',
     :class='{ invalid: (v.$dirty && !v.required) || (v.$dirty && !v.minLength) || !v.passwordRule }'
   )
     //- @change='passwordBlur',
