234f4f1d90c1cdd7e0164bd0a45eab3ecde85169
Семин Евгений <johnskyi2@yandex.ru>
2021-11-11 17:38:34 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,9 @@ module.exports = {
       proxy: { 
         "^/api/*": {
           target: 'https://45.134.255.54:8086/',
-          changeOrigin: true
-        }
+          changeOrigin: true,
+          ws: true,
+        },
       },
       
     },
