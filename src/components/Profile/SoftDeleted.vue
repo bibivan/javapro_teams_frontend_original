72d18b8d768bde36eebbd734ff39cc34f769e3a8
teamGro <tima.groshev@yandex.ru>
2021-12-21 00:40:10 +0300

    commit

---
+++ src/components/Profile/SoftDeleted.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile-info">
+    <div class="profile-info__pic">
+
+      <div
+        class="profile-info__img"
+        :class='{ offline: !online && !me }'
+      >
+        <img
+          :src='info.photo'
+          :alt='info.fullName'
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'ProfileDeleted',
+  props: {
+    me: Boolean,
+    info: Object,
+    online: Boolean
+  },
+  computed: {
+    ...mapGetters('profile/dialogs', ['dialogs'])
+  }
+}
+</script>
